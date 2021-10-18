df3b08768558d68e9ccc994488184a1765172807
Vadim Ledyaev <vl@pmobi.tech>
2021-10-18 21:28:02 +0700

    Test build

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:nekoton_flutter/nekoton_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +11,25 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Plugin example app'),
           ),
-          body: const Center(),
+          body: Center(
+            child: RaisedButton(
+              child: const Text('PRESS ME'),
+              onPressed: () async {
+                final instance = await Nekoton.getInstance();
+
+                final wallet = await instance.subscriptionsController.subscribeToGenericContract(
+                  origin: "origin",
+                  address: "0:f35f602c47bf42c3e292262023aa7e71a53e604fdf6bf42be4bf6dd9ab8e04c3",
+                );
+
+                wallet.onTransactionsFoundStream.listen((event) {
+                  if (event.lastOrNull?.prevTransactionId != null) {
+                    wallet.preloadTransactions(event.lastOrNull!.prevTransactionId!);
+                  }
+                });
+              },
+            ),
+          ),
         ),
       );
 }
